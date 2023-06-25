8344cf200f8263ed9718d10bce5aa0f20540e344
ranjit95 <ranjit95@gmail.com>
2023-06-26 01:45:37 +0600

    add es6

---
+++ ES6.js
@@ -0,0 +1,7 @@
+const myArray = ["ranjti", "kajal"];
+//"strict" keyword is used to continue strict . strict as a police in js 
+
+// ...arrayName is also called spread Operator
+
+
+// ...rest parameter 
